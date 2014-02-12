8a5c523447676f9bb672b0021547ff2792a1effc
Hillfolk <pjy1418@gmail.com>
2014-02-12 14:33:26 +0000

    aws setting

--- menutab/menutab/urls.py
+++ menutab/menutab/urls.py
@@ -6,8 +6,7 @@ urlpatterns = patterns('',
                        # Examples:
                        # url(r'^$', 'menutab.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
-
-					   url(r'', include('gcm.urls')),
+		       url(r'', include('gcm.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^push_app/', include('push_app.urls')),
                        url(r'^order_app/', include('order_app.urls')),
