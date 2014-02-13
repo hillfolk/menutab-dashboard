688e722628000c4266dc1d921a1752d01983a849
Hillfolk <pjy1418@gmail.com>
2014-02-13 00:53:12 +0000

    -a

--- menutab/menutab/urls.py
+++ menutab/menutab/urls.py
@@ -4,7 +4,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
                        # Examples:
-                       # url(r'^$', 'menutab.views.home', name='home'),
+                        url(r'^$', 'menutab.views.home', name='home'),
                        # url(r'^blog/', include('blog.urls')),
 		       url(r'', include('gcm.urls')),
                        url(r'^admin/', include(admin.site.urls)),
