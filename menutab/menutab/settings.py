4a5699f76c2cd0d52f6e8b9bbb11fc783da3c010
Hillfolk <pjy1418@gmail.com>
2014-02-13 15:24:00 +0000

    conf

--- menutab/menutab/settings.py
+++ menutab/menutab/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'menutab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangostack',
+        'HOST': '/opt/bitnami/postgresql',
+        'PORT': '5432',
+        'USER': 'bitnami',
+        'PASSWORD': '3fc46b800d'
     }
 }
 
