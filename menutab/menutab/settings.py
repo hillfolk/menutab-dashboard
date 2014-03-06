5fa441c8e0b62cfc15a631ac1e5cbc16ff9ca8f9
Hillfolk <pjy1418@gmail.com>
2014-03-06 14:01:30 +0900

    프로덕션 셋팅 및 customer_key 항목 모델 삭제

--- menutab/menutab/settings.py
+++ menutab/menutab/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'dashboard',
-        'HOST': 'menutab-dashboard.cxfa44dybjaj.ap-northeast-1.rds.amazonaws.com',
+        'HOST': 'menutab-dashboard.chsffw8ul0ti.ap-northeast-1.rds.amazonaws.com',
         'PORT': '3306',
         'USER': 'dashboard',
         'PASSWORD': 'mark130620'
